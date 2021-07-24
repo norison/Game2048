a68c5acb28c747992a39a4cf2478eaabc8ff486f
Ihor Matiev <ihor.matiev@gmail.com>
2021-07-24 13:15:45 +0300

    Add project files.

---
+++ Game2048.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Game2048.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
